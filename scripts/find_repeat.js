90f0fadb81b0b6498dae7773518583845528ecc3
Cherry Ng <cherry92@gmail.com>
2021-05-03 09:34:03 +0800

    Update script to find repeated keys.

--- scripts/find_repeat.js
+++ scripts/find_repeat.js
@@ -1,11 +1,19 @@
+import argparse from 'argparse';
+import fs from 'fs';
 import readline from 'readline';
+
+const arg_parser = new argparse.ArgumentParser({});
+arg_parser.add_argument('--cj3ext', {help: 'Additional extension dictionary.'});
+const args = arg_parser.parse_args();
 
 let is_dict = false;
 let encoding = '';
 let repeated = [];
 
-const rl = readline.createInterface(process.stdin);
-rl.on('line', function (line) {
+let CJ3EXT = {};
+
+const cj3ext_rl = readline.createInterface(fs.createReadStream(args.cj3ext));
+cj3ext_rl.on('line', function (line) {
   if (!is_dict || line === '') {
     console.log(line);
   }
@@ -17,15 +25,38 @@ rl.on('line', function (line) {
     let tabs = line.split('\t');
     const word = tabs[0];
     const keys = tabs[1];
-    if (keys !== encoding) {
-      if (repeated.length > 1) {
-        repeated.sort().forEach(function (w) {
-          console.log(`${w}\t${encoding}`);
-        });
-      }
-      encoding = keys;
-      repeated = [];
+    if (!CJ3EXT.hasOwnProperty(keys)) {
+      CJ3EXT[keys] = [];
     }
-    repeated.push(word);
+    CJ3EXT[keys].push(word);
   }
+}).on('close', function () {
+  const rl = readline.createInterface(process.stdin);
+  rl.on('line', function (line) {
+    if (!is_dict || line === '') {
+      console.log(line);
+    }
+    if (line === '...') {
+      is_dict = true;
+      return;
+    }
+    if (is_dict && line !== '') {
+      let tabs = line.split('\t');
+      const word = tabs[0];
+      const keys = tabs[1];
+      if (keys !== encoding) {
+        if (CJ3EXT.hasOwnProperty(encoding)) {
+          repeated = repeated.concat(CJ3EXT[encoding]);
+        }
+        if (repeated.length > 1) {
+          repeated.forEach(function (w) {
+            console.log(`${w}\t${encoding}`);
+          });
+        }
+        encoding = keys;
+        repeated = [];
+      }
+      repeated.push(word);
+    }
+  });
 });
